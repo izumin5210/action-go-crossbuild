6f2ee7598840c7e71e91aed6580e209afc1162d5
izumin5210 <m@izum.in>
2019-10-02 23:17:15 +0900

    Impl

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,55 @@
+import * as path from 'path';
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as tc from '@actions/tool-cache';
 import {wait} from './wait'
+
+const inputByGoxzFlag = {
+  arch:            'arch',
+  os:              'os',
+  'build-ldflags': 'ldflags',
+  'build-tags':    'tags',
+  d:               'dest',
+  n:               'name',
+};
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const goxzVersion = core.getInput('goxz-version');
+    const goxzPath = await getGoxzPath(goxzVersion);
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+    await exec.exec(goxzPath, [
+      ...buildFlags(),
+      core.getInput("package"),
+    ]);
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function getGoxzPath(version: string): Promise<string> {
+  return path.join(tc.find('goxz', version) || await downloadGoxz(version), "goxz");
+}
+
+async function downloadGoxz(version: string): Promise<string> {
+  const archivePath = await tc.downloadTool(getUrl(version));
+  const extractedPath = await tc.extractTar(archivePath);
+  return tc.cacheDir(extractedPath, "goxz", version);
+}
+
+function getUrl(version: string): string {
+  return `https://github.com/Songmu/goxz/releases/download/${version}/${getArchiveName(version)}.tar.gz`
+}
+
+function getArchiveName(version: string): string {
+  return `goxz_${version}_linux_amd64`
+}
+
+function buildFlags(): Array<string> {
+  return Object.entries(inputByGoxzFlag)
+    .map(([k, v]) => [k, core.getInput(v)])
+    .filter(([_, v]) => v.length > 0)
+    .map(([k ,v]) => `--${k}="${v}"`);
 }
 
 run();
